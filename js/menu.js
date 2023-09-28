d70347c8424e11eb4e40484f17dfe1143a890cba
João Carlos <joao_carlos_jr@hotmail.com>
2023-09-27 21:36:31 -0300

    feito

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+document.querySelector('#menu-icon').addEventListener("click", () => {
+        document.querySelector("#menu").classList.toggle("open")
+})
+
+
+document.querySelector('#menu-icon').addEventListener("click", () => {
+    document.querySelector("#pesquisa-div").classList.toggle("pesquisa")
+})
+
+
